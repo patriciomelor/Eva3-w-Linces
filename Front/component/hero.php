917658aa6dc042514c0862da36842f02bb4792ba
CaroVeero <caro.bohle@gmail.com>
2024-05-27 19:16:42 -0400

    Contenido de Hero

--- Front/component/hero.php
+++ Front/component/hero.php
@@ -2,37 +2,37 @@
   <div class="carousel-inner"  style="margin-left:auto; margin-right:auto; margin-bottom: 30px;">
     <!--Primera slide-->
     <div class="carousel-item active" >
-      <div class="row-cards text-center" style="margin-left:150px; margin-right:150px"  alt="First slide" >
+      <div class="row-cards text-center" style="margin-left:170px; margin-right:170px"  alt="First slide" >
         <div class="card-header" style="margin-bottom:40px; margin-top:100px" >
           <img src="src/media/carrousel/Rostro1.jpg" alt="...">
         </div>
         <div class="card-body">
-          <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, quaerat nisi fugit fuga veritatis officiis. Expedita, dicta! Odit dolore earum commodi iure sunt blanditiis debitis nostrum, libero, mollitia voluptatibus deserunt!</p>
-          <p class="card-text">NPersona1</p>
+          <p class="card-text">"¡Una inversión que vale la pena! Comprar nuestro terreno con ustedes fue una experiencia gratificante. La ubicación es perfecta y el valor que recibimos por nuestro dinero es inigualable. Estamos emocionados por las oportunidades que ofrece este lugar y agradecidos por su excelente servicio al cliente."</p>
+          <p class="card-text">Patricio Muñoz</p>
         </div>
       </div>
     </div>
     <!--Segunda slide-->
     <div class="carousel-item" >
-      <div class="row-cards text-center" style="margin-left:150px; margin-right:150px"  alt="Second slide" >
+      <div class="row-cards text-center" style="margin-left:170px; margin-right:170px"  alt="Second slide" >
         <div class="card-header" style="margin-bottom:40px; margin-top:100px" >
           <img src="src/media/carrousel/Rostro2.jpg" alt="...">
         </div>
         <div class="card-body">
-          <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, quaerat nisi fugit fuga veritatis officiis. Expedita, dicta! Odit dolore earum commodi iure sunt blanditiis debitis nostrum, libero, mollitia voluptatibus deserunt!</p>
-          <p class="card-text">NPersona2</p>
+          <p class="card-text">"¡No podríamos estar más felices con nuestra nueva casa! Gracias al equipo por su profesionalismo y dedicación. Cada detalle fue cuidadosamente considerado, todo el proceso superó nuestras expectativas. ¡Definitivamente recomendaremos sus servicios a amigos y familiares!"</p>
+          <p class="card-text">Daniela Salinas</p>
         </div>
       </div>
     </div>
     <!--Tercera slide-->
     <div class="carousel-item" >
-      <div class="row-cards text-center" style="margin-left:150px; margin-right:150px"  alt="Third slide" >
+      <div class="row-cards text-center" style="margin-left:170px; margin-right:170px"  alt="Third slide" >
         <div class="card-header" style="margin-bottom:40px; margin-top:100px" >
           <img src="src/media/carrousel/Rostro3.jpg" alt="...">
         </div>
         <div class="card-body">
-          <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, quaerat nisi fugit fuga veritatis officiis. Expedita, dicta! Odit dolore earum commodi iure sunt blanditiis debitis nostrum, libero, mollitia voluptatibus deserunt!</p>
-          <p class="card-text">NPersona3</p>
+          <p class="card-text">"¡Increíble experiencia! La compra de nuestra parcela con ustedes fue sin duda una decisión acertada. Desde el primer contacto hasta la entrega, el proceso fue transparente y eficiente. ¡Estamos ansiosos por comenzar a construir nuestro hogar en este hermoso lugar!"</p>
+          <p class="card-text">Carolina Pizarro</p>
         </div>
       </div>
     </div>
